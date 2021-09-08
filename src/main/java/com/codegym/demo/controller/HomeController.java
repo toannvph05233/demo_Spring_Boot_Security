2d8ec677bc26b141bcbfe6a0de56da129eff3e71
toannvph05233 <johntoan98@gmail.com>
2021-09-08 16:43:54 +0700

    upload file

--- src/main/java/com/codegym/demo/controller/HomeController.java
+++ src/main/java/com/codegym/demo/controller/HomeController.java
@@ -2,12 +2,24 @@ package com.codegym.demo.controller;
 
 import com.codegym.demo.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 public class HomeController {
+    @Value("${uploadPart}")
+    String uploadPart;
+
+
     @Autowired
     IProductService productService;
 
@@ -17,8 +29,26 @@ public class HomeController {
         modelAndView.addObject("products", productService.findAll());
         return modelAndView;
     }
+
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/upload")
+    public String uploadfile() {
+        return "uploadfile";
+    }
+
+    @PostMapping("/upload")
+    public ModelAndView upload(@RequestParam MultipartFile file) throws IOException {
+
+        String name = file.getOriginalFilename();
+        FileCopyUtils.copy(file.getBytes(), new File(uploadPart + name));
+
+        ModelAndView modelAndView = new ModelAndView("uploadfile");
+        modelAndView.addObject("img", name);
+
+        return modelAndView;
     }
 }
